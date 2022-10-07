72b79709881916f570aae7f76cfa604315602a6b
aimen <aimen.cherif@etu.univ-amu.fr>
2022-10-07 19:51:52 +0200

    fix

--- snpapp/admin.py
+++ snpapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import SNP, Reference, Disease_Trait, SNP_Phenotype_Reference
 
-admin.site.register(SNP, Reference, Disease_Trait, SNP_Phenotype_Reference)
+admin.site.register(SNP)
+admin.site.register(Reference)
+admin.site.register(Disease_Trait)
+admin.site.register(SNP_Phenotype_Reference)
