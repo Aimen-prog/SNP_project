f733122f08675a4e9ace7f1e8a146decb4bfd942
aimen <aimen.cherif@etu.univ-amu.fr>
2022-10-07 19:44:16 +0200

    database settings

--- snpapp/admin.py
+++ snpapp/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from .models import SNP, Reference, Disease_Trait, SNP_Phenotype_Reference
+
+admin.site.register(SNP, Reference, Disease_Trait, SNP_Phenotype_Reference)
